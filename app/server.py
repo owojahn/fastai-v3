14d8e0ffa3879ced799642cf1965ba1c19212db5
owojahn <49555183+owojahn@users.noreply.github.com>
2019-07-15 12:16:01 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -273,8 +273,7 @@ async def analyze(request):
     while outputs[idxnew[-i].item()].item() > 0.05 and i < 5:
       wkeit = round(100*outputs[idxnew[-i].item()].item(),2)
       prediction = cat[idxnew[-i].item()]
-      return JSONResponse({'result': str(prediction)})
-      return JSONResponse({'wkeit': str(wkeit)})
+      return JSONResponse({'result': str(prediction),'wkeit': str(wkeit)})
       i +=1
 
 
